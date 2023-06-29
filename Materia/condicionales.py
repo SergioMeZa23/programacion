d635735e08a9f9ca26882cb2fb86ad0a1ef9efba
SergioMeZa23 <129112027+SergioMeZa23@users.noreply.github.com>
2023-06-28 23:01:11 -0400

    Add files via upload

---
+++ Materia/condicionales.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+licencia = False
+edad = 19
+automovil = False
+
+
+if licencia:
+    print('Puedo conducir porque tengo licencia\n')
+else:
+    print('No tengo licencia para conducir\n')
+print("Este print esta fuera del else")
+
+
+
+if edad:
+    print('Puedo conducir porque soy mayor de edad\n')
+else:
+    print('No puedo conducir soy menor de edad\n')
+    
+
+if edad >= 18:
+    print('Puedo conducir porque soy mayor de edad\n')
+else:
+    print('No puedo conducir soy menor de edad\n')
+
+
+
+if licencia and edad >= 18:
+    print('Puedo conducir porque soy mayor de edad y tengo licencia')
+elif automovil:
+    print('Tengo automovil, pero no tengo licencia ni la edad necesaria')
+else:
+    print('No puedo conducir no tengo ni la edad, ni licencia ni automovil')
+
