294bd8ae7cb90a8d637c42fac65f683339e18778
SergioMeZa23 <129112027+SergioMeZa23@users.noreply.github.com>
2023-06-28 23:11:58 -0400

    Add files via upload

---
+++ Materia/condicionales_2.py
@@ -0,0 +1,30 @@
+
+x = 15
+if x > 10:
+  print('x es mayor que 10')
+
+x = 5
+if x > 10:
+ print('x es mayor que 10')
+else:
+  print('x es menor o igual que 10')
+
+
+x = 7
+if x > 10:
+  print('x es mayor que 10')
+elif x < 10:
+ print('x es menor que 10')
+else:
+ print('x es igual a 10')
+
+
+x = 11
+if x > 5:
+ print('x es mayor que 5')
+elif x > 10:
+ print('x es mayor que 10')
+else:
+ print('x es menor que 5')
+
+
